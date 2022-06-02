64a65fad3684bc4966b39b01daa493b0fa96cf19
Deepesh Gupta <34032722+PrinceDeepu@users.noreply.github.com>
2022-06-02 18:34:30 +0530

    added cookie parser and helmet for every api request

--- src/middlewares/app.ts
+++ src/middlewares/app.ts
@@ -1,6 +1,8 @@
 import { Logger } from "../lib/Log/Logger";
 import { Express, json, urlencoded } from "express";
 import { unhandledErrors } from "../events/unhandledErrors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
 
 const debug: Logger = new Logger({ moduleName: "noddy:middlewares" });
 
@@ -10,6 +12,16 @@ export const middlewares = (app: Express): Express => {
      */
     unhandledErrors();
     debug.debug("Registered Unhandled Errors nand Rejections");
+
+    /**
+     * set cookie parser middleware
+     */
+    app.use(cookieParser());
+
+    /**
+     * Using helmet for securing headers
+     */
+    app.use(helmet());
 
     app.use(json());
     app.use(urlencoded({ extended: false }));
